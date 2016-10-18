931ec724723f8002c0fa7fc11f457ca57dbe447e
Emily Chen <Emily Chen>
2016-10-18 15:11:58 -0700

    Commit Description

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -8,19 +8,19 @@ devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
 
-
+library("fueleconomy")
 # You should have have access to the `vehicles` data.frame
-
+View(vehicles)
 
 # Create a data.frame of vehicles from 1997
-
+vehicles.1997<- data.frame(vehicles[vehicles$year == 1997,])
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
 
-
+unique(vehicles.1997$year)
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
 
-
+two.wheel.20.mpg<- vehicles[vehicles$drive == "2-Wheel Drive"]
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
 
 
@@ -29,5 +29,6 @@ devtools::install_github("hadley/fueleconomy")
 
 
 # What was the most efficient honda model of 1995?
+
 
 
